0db0f4a2f50182664ea96e35c2cf0aaa19ef04c0
Gustavo Martínez <tavo962@gmail.com>
2017-07-15 16:53:03 -0500

    Spotify Fix

--- install-spotify.sh
+++ install-spotify.sh
@@ -7,13 +7,17 @@
 #  ╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝
 
 # 1. Add the Spotify repository signing key to be able to verify downloaded packages
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
 
 # 2. Add the Spotify repository
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
 
 # 3. Update list of available packages
-sudo apt-get update
+apt update
 
-# 4. Install Spotify
-sudo apt-get install spotify-client
+# 4. Install the downloaded package (for example libssl1.0.0_1.0.2d-1_amd64.deb)
+# Download libssl1.0.0 package from https://packages.debian.org/jessie/libssl1.0.0
+apt install ./libssl1.0.0_1.0.2d-1_amd64.deb
+
+# 5. Install Spotify
+apt install spotify-client
